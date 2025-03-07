28b44ce607619eeec1a5647388a6a4f04acb3a01
Andrezs9 <andreslopsz99@gmail.com>
2025-03-06 21:05:56 -0500

    Add files via upload

---
+++ Ejercicio1_area.py
@@ -0,0 +1,6 @@
+base = float(input("Ingrese el valor de la base: "))
+altura = float(input("Ingrese el valor de la altura: "))
+
+area=base*altura
+
+print (f"El área del rectangulo es de: {area:.2f}")
